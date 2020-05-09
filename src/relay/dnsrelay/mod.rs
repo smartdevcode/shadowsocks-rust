9b0543d17d05ae0b4e5a03de74db752688dfc604
SeniorDevCode <senior.dev.software@gmail.com>
2020-05-09 23:54:55 +0800

    [#243] Casts rlimit fields to rlim_t for compatibility

--- src/relay/dnsrelay/mod.rs
+++ src/relay/dnsrelay/mod.rs
@@ -1,90 +1,194 @@
 use std::{
     io,
     net::{IpAddr, SocketAddr},
-    sync::Arc,
     time::Duration,
 };
 
-use tokio::sync::mpsc;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    sync::mpsc,
+};
+
+#[cfg(target_os = "android")]
+use std::path::PathBuf;
+#[cfg(target_os = "android")]
+use tokio::net::UnixStream;
 
 #[cfg(not(target_os = "android"))]
 use std::net::{Ipv4Addr, SocketAddrV4};
 #[cfg(not(target_os = "android"))]
 use tokio::net::UdpSocket;
 
-use log::{debug, error, info, warn};
+use byteorder::{BigEndian, ByteOrder};
+use log::{debug, error, info};
+use rand::Rng;
 use trust_dns_proto::{
     op::{header::MessageType, response_code::ResponseCode, Message, Query},
-    rr::RData,
+    rr::{Name, RData, RecordType},
 };
 
 use crate::{
-    config::ConfigType,
+    config::{ConfigType, ServerConfig},
     context::SharedContext,
     relay::{
         loadbalancing::server::{PlainPingBalancer, ServerType},
+        socks5::Address,
         sys::create_udp_socket,
+        tcprelay::ProxyStream,
         utils::try_timeout,
     },
 };
 
-mod upstream;
-
-async fn acl_lookup<Local, Remote>(
-    context: SharedContext,
-    local: Arc<Local>,
-    remote: Arc<Remote>,
-    query: &Query
-) -> (io::Result<Message>, bool)
-    where
-        Local: upstream::Upstream,
-        Remote: upstream::Upstream,
+async fn stream_lookup<T>(qname: &Name, qtype: RecordType, stream: &mut T) -> io::Result<Message>
+where
+    T: AsyncReadExt + AsyncWriteExt + Unpin,
 {
+    let mut message = Message::new();
+    let mut query = Query::new();
+
+    query.set_query_type(qtype);
+    query.set_name(qname.clone());
+
+    let id = rand::thread_rng().gen();
+    message.set_id(id);
+    message.set_recursion_desired(true);
+    message.add_query(query);
+
+    let req_buffer = message.to_vec()?;
+    let size = req_buffer.len();
+    let mut send_buffer = vec![0; size + 2];
+
+    BigEndian::write_u16(&mut send_buffer[0..2], size as u16);
+    send_buffer[2..size + 2].copy_from_slice(&req_buffer[0..size]);
+    stream.write_all(&send_buffer[0..size + 2]).await?;
+
+    let mut res_buffer = vec![0; 2];
+    stream.read_exact(&mut res_buffer[0..2]).await?;
+
+    let size = BigEndian::read_u16(&res_buffer[0..2]) as usize;
+    let mut res_buffer = vec![0; size];
+    stream.read_exact(&mut res_buffer[0..size]).await?;
+
+    Ok(Message::from_vec(&res_buffer)?)
+}
+
+#[cfg(target_os = "android")]
+async fn local_lookup(qname: &Name, qtype: RecordType, path: &PathBuf) -> io::Result<Message> {
+    let mut stream = UnixStream::connect(path).await?;
+    stream_lookup(qname, qtype, &mut stream).await
+}
+
+#[cfg(not(target_os = "android"))]
+async fn local_lookup(qname: &Name, qtype: RecordType, server: &SocketAddr) -> io::Result<Message> {
+    let bind_addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0));
+    let mut socket = UdpSocket::bind(bind_addr).await?;
+
+    let mut message = Message::new();
+    let mut query = Query::new();
+
+    query.set_query_type(qtype);
+    query.set_name(qname.clone());
+
+    let id = rand::thread_rng().gen();
+    message.set_id(id);
+    message.set_recursion_desired(true);
+    message.add_query(query);
+
+    let req_buffer = message.to_vec()?;
+    socket.send_to(&req_buffer, server).await?;
+
+    let mut res_buffer = vec![0; 512];
+    socket.recv_from(&mut res_buffer).await?;
+
+    Ok(Message::from_vec(&res_buffer)?)
+}
+
+async fn proxy_lookup(
+    context: SharedContext,
+    svr_cfg: &ServerConfig,
+    ns: &Address,
+    qname: &Name,
+    qtype: RecordType,
+) -> io::Result<Message> {
+    let mut stream = ProxyStream::connect_proxied(context, svr_cfg, ns).await?;
+    stream_lookup(qname, qtype, &mut stream).await
+}
+
+async fn acl_lookup(
+    context: SharedContext,
+    svr_cfg: &ServerConfig,
+    #[cfg(target_os = "android")] local: &PathBuf,
+    #[cfg(not(target_os = "android"))] local: &SocketAddr,
+    remote: &Address,
+    qname: &Name,
+    qtype: RecordType,
+) -> io::Result<(Message, bool)> {
     // Start querying name servers
     debug!(
         "attempting lookup of {:?} {} with ns {:?} and {:?}",
-        query.query_type(), query.name(), local, remote
+        qtype, qname, local, remote
     );
 
-    let remote_response_fut = try_timeout(remote.lookup(query), Some(Duration::new(3, 0)));
-    let local_response_fut = try_timeout(local.lookup(query), Some(Duration::new(3, 0)));
+    // remove the last dot from fqdn name
+    let mut name = qname.to_ascii();
+    name.pop();
+    let addr = Address::DomainNameAddress(name, 0);
+    let qname_in_proxy_list = context.check_qname_in_proxy_list(&addr);
 
-    match context.check_query_in_proxy_list(query) {
+    let remote_response_fut = async {
+        match qname_in_proxy_list {
+            Some(false) => None,
+            _ => {
+                let timeout = Some(Duration::new(3, 0));
+                try_timeout(proxy_lookup(context.clone(), svr_cfg, remote, qname, qtype), timeout)
+                    .await
+                    .ok()
+            }
+        }
+    };
+
+    let local_response = match qname_in_proxy_list {
+        Some(true) => None,
+        _ => {
+            let timeout = Some(Duration::new(3, 0));
+            try_timeout(local_lookup(qname, qtype, local), timeout).await.ok()
+        }
+    }
+    .unwrap_or_else(Message::new);
+
+    match qname_in_proxy_list {
         Some(true) => {
-            let remote_response = remote_response_fut.await;
+            let remote_response = remote_response_fut.await.unwrap_or_else(Message::new);
             debug!("pick remote response (qname): {:?}", remote_response);
-            return (remote_response, true);
+            return Ok((remote_response, true));
         }
         Some(false) => {
-            let local_response = local_response_fut.await;
             debug!("pick local response (qname): {:?}", local_response);
-            return (local_response, false);
+            return Ok((local_response, false));
         }
         None => (),
     }
 
-    // FIXME: spawn(remote_response_fut)
-    let local_response = local_response_fut.await.unwrap_or_else(|_| Message::new());
+    if local_response.answer_count() == 0 {
+        let remote_response = remote_response_fut.await.unwrap_or_else(Message::new);
+        return Ok((remote_response, true));
+    }
+
     for rec in local_response.answers() {
-        if rec.record_type() != query.query_type() {
-            warn!("local DNS response has inconsistent answer type {} for query {}", rec.record_type(), query);
-            break
-        }
         let forward = match rec.rdata() {
             RData::A(ref ip) => context.check_ip_in_proxy_list(&IpAddr::from(*ip)),
             RData::AAAA(ref ip) => context.check_ip_in_proxy_list(&IpAddr::from(*ip)),
-            RData::PTR(_) => panic!("PTR records should not reach here"),
-            _ => context.is_default_in_proxy_list(),
+            _ => true,
         };
         if !forward {
-            debug!("pick local response (response): {:?}", local_response);
-            return (Ok(local_response), false);
+            debug!("pick local response (ip): {:?}", local_response);
+            return Ok((local_response, false));
         }
     }
 
-    let remote_response = remote_response_fut.await;
-    debug!("pick remote response (response): {:?}", remote_response);
-    (remote_response, true)
+    let remote_response = remote_response_fut.await.unwrap_or_else(Message::new);
+    debug!("pick remote response (ip): {:?}", remote_response);
+    Ok((remote_response, true))
 }
 
 /// Start a DNS relay local server
@@ -121,22 +225,8 @@ pub async fn run(context: SharedContext) -> io::Result<()> {
         }
     });
 
-    let config = context.config();
-    #[cfg(target_os = "android")]
-    let local_upstream = Arc::new(upstream::UnixSocketUpstream {
-        path: config.local_dns_path.clone().expect("local query DNS path"),
-    });
-    #[cfg(not(target_os = "android"))]
-    let local_upstream = Arc::new(upstream::UdpUpstream {
-        server: config.local_dns_addr.clone().expect("local query DNS address"),
-    });
     // FIXME: We use TCP to send remote queries by default, which should be configuable.
     let balancer = PlainPingBalancer::new(context.clone(), ServerType::Tcp).await;
-    let remote_upstream = Arc::new(upstream::ProxyTcpUpstream {
-        context: context.clone(),
-        svr_cfg: move || balancer.pick_server().server_config().clone(),
-        ns: config.remote_dns_addr.clone().expect("remote query DNS address"),
-    });
 
     loop {
         let mut req_buffer: [u8; 512] = [0; 512];
@@ -159,9 +249,8 @@ pub async fn run(context: SharedContext) -> io::Result<()> {
         debug!("received src: {}, query: {:?}", src, request);
 
         let context = context.clone();
-        let local_upstream = Arc::clone(&local_upstream);
-        let remote_upstream = Arc::clone(&remote_upstream);
         let mut qtx = qtx.clone();
+        let server = balancer.pick_server();
 
         tokio::spawn(async move {
             let mut message = Message::new();
@@ -173,10 +262,24 @@ pub async fn run(context: SharedContext) -> io::Result<()> {
             if request.queries().is_empty() {
                 message.set_response_code(ResponseCode::FormErr);
             } else {
-                let question = &request.queries()[0];
-                let (r, forward) = acl_lookup(context.clone(), local_upstream, remote_upstream, question).await;
+                let config = context.config();
 
-                if let Ok(result) = r {
+                #[cfg(target_os = "android")]
+                let local_addr = config.local_dns_path.as_ref().expect("local query DNS path");
+                #[cfg(not(target_os = "android"))]
+                let local_addr = config.local_dns_addr.as_ref().expect("local query DNS address");
+
+                let remote_addr = config.remote_dns_addr.as_ref().expect("remote query DNS address");
+
+                let question = &request.queries()[0];
+
+                let qname = question.name();
+                let qtype = question.query_type();
+                let svr_cfg = server.server_config();
+
+                let r = acl_lookup(context.clone(), svr_cfg, &local_addr, &remote_addr, qname, qtype).await;
+
+                if let Ok((result, forward)) = r {
                     for rec in result.answers() {
                         debug!("dns answer: {:?}", rec);
 
